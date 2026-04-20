11f86cc1b21166d7c1439f8ed629e20a57b5fe5a
thomas bartier <thomas.bartier@gmail.com>
2026-04-20 13:47:40 +0200

    clean code

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -9,9 +9,7 @@ const AuthPage = () => {
         <div className="flex min-h-svh p-6">
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
         <div>
-          <h1 className="font-medium">Project ready! Branch dev</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
+          <h1 className="font-medium">MonBasketball ! ⚠️ En développement ! ⚠️</h1>
           <img src={icon} alt="" />
           <Button className="mt-2" onClick={() => navigate("/home")}>GO HOME!</Button>
         </div>
