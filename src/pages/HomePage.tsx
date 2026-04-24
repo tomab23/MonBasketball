160a958ac988c09aa1a3065ce04a64b586063214
thomas bartier <thomas.bartier@gmail.com>
2026-04-24 14:48:46 +0200

    add helper date + add stat homePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,14 @@
 import { Button } from "@/components/ui/button"
+import { useAuth } from "@/context/AuthContext";
+import { stringToDate } from "@/helpers/DateFormat";
+import { useSession } from "@/hooks/useSession";
 import { useNavigate } from "react-router-dom"
 
 const HomePage = () => {
   const navigate = useNavigate();
+  const { user } = useAuth();
+  const { sessions } = useSession();
+
   return (
     <div className="contenu">
       HOME PAGE
@@ -10,6 +16,9 @@ const HomePage = () => {
       <Button onClick={() => navigate("/4")}>404</Button>
       <Button onClick={() => navigate("/dashboard")}>Dashboard test</Button>
       <Button onClick={() => navigate("/form")}>Session form test</Button>
+
+      <br /><br /><br />
+      Nombre de sessions depuis le <i>{stringToDate(user?.created_at)}</i> : <b>{sessions.length}</b>
     </div>
   )
 }
